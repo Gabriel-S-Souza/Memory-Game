ee259049349d5eb23ff8f615105e018d6291f574
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-01-21 13:29:11 -0300

    Finished boardGame object

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,8 +1,13 @@
+import "./style.css"
 import cardGame from "../../components/CardGame"
 
 function boardGame(cards) {
-    const $htmlBoardGame = cardGame().repeat(cards)
-    return $htmlBoardGame
+    const $htmlContentBoardGame = cardGame().repeat(cards)
+    return `
+        <section class="board-game">
+            ${$htmlContentBoardGame}
+        </section>
+        ` 
 }
 
 export default boardGame
