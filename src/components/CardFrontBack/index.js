4b2436fbb501c920370a1fb96b86496f51939c57
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-01-22 01:57:13 -0300

    added images of cards and updated style and logic

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -3,8 +3,14 @@ import cardGame from "../CardGame";
 function cardFrontBack() {
     return /*html*/`
         <article class="card-front-back">
-            ${cardGame()}
-            ${cardGame()}
+            ${cardGame("javascript")}
+            ${cardGame("java")}
+            ${cardGame("kotlin")}
+            ${cardGame("github")}
+            ${cardGame("python")}
+            ${cardGame("sql")}
+            ${cardGame("css")}
+            ${cardGame("php")}
         </article>
     `
 }
