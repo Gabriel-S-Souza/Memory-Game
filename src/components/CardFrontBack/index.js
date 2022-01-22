e642e3f3fb8fbb8f5c8f4da5d7041ffefccf7f62
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-01-22 05:02:14 -0300

    implemented spin on all cards

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,9 +1,15 @@
 import "./style.css"
 import cardGame from "../CardGame"
 
+window.cardFrontBack = {}
+window.cardFrontBack.handleClick = (event) => {
+    const $origin = event.target.closest(".card-front-back").classList.toggle("active")
+    console.log($origin)
+}
+
 function cardFrontBack() {
     return /*html*/`
-        <article class="card-front-back">
+        <article class="card-front-back" onclick="cardFrontBack.handleClick(event)">
             <div class="card -front">
                 ${cardGame("")}
             </div>
--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,8 +1,8 @@
 import "./style.css"
 import cardFrontBack from "../../components/CardFrontBack"
 
-function boardGame() {
-    const $htmlContentBoardGame = cardFrontBack()
+function boardGame(number) {
+    const $htmlContentBoardGame = cardFrontBack().repeat(number)
     return `
         <section class="board-game">
             ${$htmlContentBoardGame}
