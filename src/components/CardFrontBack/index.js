6b438294aa858b10f90a7ee7295fb4de915c7704
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-01-23 02:11:55 -0300

    shuffled the cards

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,18 +1,26 @@
 import "./style.css"
 import cardGame from "../CardGame"
 
+let icons = [
+    "angular", "c", "css", "dart", "flutter", "github", "html",
+    "java", "javascript", "kotlin", "node", "php", "python", "react-native",
+    "react", "sql", "swift", "typescript", "vue"
+]
+
 function getIconNames() {
-    const icons = [
-        "angular", "c", "css", "dart", "flutter", "github", "html",
-        "java", "javascript", "kotlin", "node", "php", "python", "react-native",
-        "react", "sql", "swift", "typescript", "vue"]
         function getRandomIntInclusive(min, max) {
             min = Math.ceil(min);
             max = Math.floor(max);
             return Math.random() * (max - min) + min;
         }
-        let index = Math.round(getRandomIntInclusive(0, 1) * 19)
-        return icons[index]
+        let index = Math.round(getRandomIntInclusive(0, 1) * icons.length)
+        let currentIcon = icons[index]
+        if(currentIcon == undefined) {
+            return getIconNames()
+        } else {
+            icons.splice(index, 1)
+            return currentIcon
+        }
 }
 
 window.cardFrontBack = {}
--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,15 +1,35 @@
 import "./style.css"
 import cardFrontBack from "../../components/CardFrontBack"
 
+function shufflesCards(contentCards) {
+    let newContentCards = contentCards
+    let contentCardsShuffled = []
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.random() * (max - min) + min
+    }
+
+    for (let index = 0; index < contentCards.length; index++) {
+        let randomCard = newContentCards[getRandomIntInclusive(0, newContentCards.length).toFixed()]
+        randomCard != undefined ? contentCardsShuffled.push(randomCard) : index - 1
+    }
+    return contentCardsShuffled.join('')
+}
+
 let count = 0
-let $htmlContentBoardGame = '<section class="board-game">'
+let $htmlContentBoardGame = []
 function boardGame(number) {
     if(count < number) {
-        $htmlContentBoardGame += cardFrontBack()
+        let currentCard = cardFrontBack()
+        $htmlContentBoardGame.push(currentCard)
+        $htmlContentBoardGame.push(currentCard)
+        console.log(count, currentCard)
         count++
         return boardGame(number)
     } else {
-        return $htmlContentBoardGame + "</section>"
+        let $boardGameShuffled = shufflesCards($htmlContentBoardGame)
+        return '<section class="board-game">' + $boardGameShuffled + "</section>"
     }
 }
 
